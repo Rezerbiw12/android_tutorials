59579fc9b634b85b3cddf84e964e96c0b15be93a
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-05-03 15:31:48 +0700

    update

---
+++ Android tutorials-15-ListView #1.Create a listview of students/code/app/src/main/java/com/example/hoangnd/androidtutorialproject/StudentAdapter.java
@@ -0,0 +1,54 @@
+package com.example.hoangnd.androidtutorialproject;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.hoangnd.androidtutorialproject.models.Student;
+
+import java.util.ArrayList;
+
+
+class StudentAdapter extends BaseAdapter {
+    Activity context;
+    ArrayList<Student> students;
+    private static LayoutInflater inflater = null;
+
+    public StudentAdapter(Activity context, ArrayList<Student> students) {
+        this.context = context;
+        this.students = students;
+        inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return students.size();
+    }
+
+    @Override
+    public Student getItem(int position) {
+        return students.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        View itemView = convertView;
+        itemView = (itemView == null) ? inflater.inflate(R.layout.list_item, null): itemView;
+        TextView textViewName = (TextView) context.findViewById(R.id.textViewName);
+        TextView textViewEmail = (TextView) context.findViewById(R.id.textViewEmail);
+        Student selectedStudent = students.get(position);
+        textViewName.setText();
+        return itemView;
+    }
+}
